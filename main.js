59513ec47b6046112834fb94ca6dbc14b5f19464
Ismoil98 <ismoil6366>
2024-01-22 15:31:01 +0500

    added

---
+++ main.js
@@ -0,0 +1,359 @@
+// HighOrderFunctions1
+// const me = {
+//   firstName: "Sultonqul",
+//   lastName: "Nortoyloqov",
+//   age: 23,
+//   languages: ["js", "python", "c++", "nodejs"],
+//   friends: ["Jamshid", "Abbos", "Jalol", "Mar'uf"],
+// };
+
+// for (const property in me) {
+//     console.log(`${property}: ${me[property]}`);
+// }
+
+
+// HighOrderFunctions2 ****
+// let name = prompt('n=')
+// function createDagree(name) {
+//     const Degree = {}
+//     for (let i=1; i<=name; i++) {
+//         Degree[i] = i*i
+//     }
+//     return {
+//         Degree    
+//     };
+// }
+// console.log(createDagree(name));
+
+
+// HighOrderFunctions3
+// let name = prompt('n=')
+// let values = 0
+// let properties = 0
+// function createDagree(name) {
+//     const Degree = {}
+//     for (let i=1; i<=name; i++) {
+//         Degree[i] = i*i
+//     }
+//     for (let i=1; i<=name; i++) {
+//         values += Degree[i]
+//         properties += i
+//     }
+//     return {
+//         Degree, values, properties
+//     };
+// }
+// console.log(createDagree(name));
+
+
+// HighOrderFunctions4
+// let str = ["Sultonqul", "Safarmurod", "O’rol", "Jahongir"]
+// function StrLength(name) {
+//     let Obj = {}
+//     for (let i=0; i<name.length; i++) {
+//         Obj[name[i]] = name[i].length
+//     }
+//     return Obj
+// }
+// let x = StrLength(str) 
+// console.log(x);
+
+
+// HighOrderFunctions5
+// let Obj = {"Apelsin": 10000, "Olma": 12000, "Mandarin": 8000, "Banan": 20000}
+// function Summ (obj) {
+//     let s = 0
+//     for (let i in obj) {
+//         s += obj[i]
+//     }
+//     return s
+// }
+// let x = Summ(Obj)
+// console.log(x);
+
+
+// HighOrderFunctions6
+// let obj = {abc: 1, 123: 2, d: 5}
+// function isSting (param) {
+//     let isStr = true
+//     for (let i in param) {
+//         if (!Number.isInteger(i)) {
+//             isStr = false
+//         }
+//     }
+//     return isStr
+// }
+// let x = isSting(obj)
+// console.log(x);
+
+
+
+
+// HighOrderFunctions7
+// const me = {
+//   firstName: "Sultonqul",
+//   lastName: "Nortoyloqov",
+//   age: 23,
+//   languages: ["js", "python", "c++", "nodejs"],
+//   friends: ["Jamshid", "Abbos", "Jalol", "Mar'uf"],
+// };
+
+// for (const property in me) {
+//     console.log(`${property}: ${me[property]}`);
+// }
+
+
+// HighOrderFunctions8
+// let name = prompt('n=')
+// function createDagree(name) {
+//     const Degree = {}
+//     for (let i=1; i<=name; i++) {
+//         Degree[i] = i*i
+//     }
+//     return {
+//         Degree    
+//     };
+// }
+// console.log(createDagree(name));
+
+
+// HighOrderFunctions9
+// let name = prompt('n=')
+// let values = 0
+// let properties = 0
+// function createDagree(name) {
+//     const Degree = {}
+//     for (let i=1; i<=name; i++) {
+//         Degree[i] = i*i
+//     }
+//     for (let i=1; i<=name; i++) {
+//         values += Degree[i]
+//         properties += i
+//     }
+//     return {
+//         Degree, values, properties
+//     };
+// }
+// console.log(createDagree(name));
+
+
+// HighOrderFunctions10
+// let str = ["Sultonqul", "Safarmurod", "O’rol", "Jahongir"]
+// function StrLength(name) {
+//     let Obj = {}
+//     for (let i=0; i<name.length; i++) {
+//         Obj[name[i]] = name[i].length
+//     }
+//     return Obj
+// }
+// let x = StrLength(str) 
+// console.log(x);
+
+
+// HighOrderFunctions11
+// let Obj = {"Apelsin": 10000, "Olma": 12000, "Mandarin": 8000, "Banan": 20000}
+// function Summ (obj) {
+//     let s = 0
+//     for (let i in obj) {
+//         s += obj[i]
+//     }
+//     return s
+// }
+// let x = Summ(Obj)
+// console.log(x);
+
+
+// HighOrderFunctions12
+// let obj = {abc: 1, 123: 2, d: 5}
+// function isSting (param) {
+//     let isStr = true
+//     for (let i in param) {
+//         if (!Number.isInteger(i)) {
+//             isStr = false
+//         }
+//     }
+//     return isStr
+// }
+// let x = isSting(obj)
+// console.log(x);
+
+
+
+// HighOrderFunctions13
+// const me = {
+//   firstName: "Sultonqul",
+//   lastName: "Nortoyloqov",
+//   age: 23,
+//   languages: ["js", "python", "c++", "nodejs"],
+//   friends: ["Jamshid", "Abbos", "Jalol", "Mar'uf"],
+// };
+
+// for (const property in me) {
+//     console.log(`${property}: ${me[property]}`);
+// }
+
+
+// HighOrderFunctions14
+// let name = prompt('n=')
+// function createDagree(name) {
+//     const Degree = {}
+//     for (let i=1; i<=name; i++) {
+//         Degree[i] = i*i
+//     }
+//     return {
+//         Degree    
+//     };
+// }
+// console.log(createDagree(name));
+
+
+// HighOrderFunctions15
+// let name = prompt('n=')
+// let values = 0
+// let properties = 0
+// function createDagree(name) {
+//     const Degree = {}
+//     for (let i=1; i<=name; i++) {
+//         Degree[i] = i*i
+//     }
+//     for (let i=1; i<=name; i++) {
+//         values += Degree[i]
+//         properties += i
+//     }
+//     return {
+//         Degree, values, properties
+//     };
+// }
+// console.log(createDagree(name));
+
+
+// HighOrderFunctions16
+// let str = ["Sultonqul", "Safarmurod", "O’rol", "Jahongir"]
+// function StrLength(name) {
+//     let Obj = {}
+//     for (let i=0; i<name.length; i++) {
+//         Obj[name[i]] = name[i].length
+//     }
+//     return Obj
+// }
+// let x = StrLength(str) 
+// console.log(x);
+
+
+// HighOrderFunctions17
+// let Obj = {"Apelsin": 10000, "Olma": 12000, "Mandarin": 8000, "Banan": 20000}
+// function Summ (obj) {
+//     let s = 0
+//     for (let i in obj) {
+//         s += obj[i]
+//     }
+//     return s
+// }
+// let x = Summ(Obj)
+// console.log(x);
+
+
+// HighOrderFunctions18
+// let obj = {abc: 1, 123: 2, d: 5}
+// function isSting (param) {
+//     let isStr = true
+//     for (let i in param) {
+//         if (!Number.isInteger(i)) {
+//             isStr = false
+//         }
+//     }
+//     return isStr
+// }
+// let x = isSting(obj)
+// console.log(x);
+
+
+
+
+// HighOrderFunctions19
+// const me = {
+//   firstName: "Sultonqul",
+//   lastName: "Nortoyloqov",
+//   age: 23,
+//   languages: ["js", "python", "c++", "nodejs"],
+//   friends: ["Jamshid", "Abbos", "Jalol", "Mar'uf"],
+// };
+
+// for (const property in me) {
+//     console.log(`${property}: ${me[property]}`);
+// }
+
+
+// HighOrderFunctions20
+// let name = prompt('n=')
+// function createDagree(name) {
+//     const Degree = {}
+//     for (let i=1; i<=name; i++) {
+//         Degree[i] = i*i
+//     }
+//     return {
+//         Degree    
+//     };
+// }
+// console.log(createDagree(name));
+
+
+// HighOrderFunctions21
+// let name = prompt('n=')
+// let values = 0
+// let properties = 0
+// function createDagree(name) {
+//     const Degree = {}
+//     for (let i=1; i<=name; i++) {
+//         Degree[i] = i*i
+//     }
+//     for (let i=1; i<=name; i++) {
+//         values += Degree[i]
+//         properties += i
+//     }
+//     return {
+//         Degree, values, properties
+//     };
+// }
+// console.log(createDagree(name));
+
+
+// HighOrderFunctions22
+// let str = ["Sultonqul", "Safarmurod", "O’rol", "Jahongir"]
+// function StrLength(name) {
+//     let Obj = {}
+//     for (let i=0; i<name.length; i++) {
+//         Obj[name[i]] = name[i].length
+//     }
+//     return Obj
+// }
+// let x = StrLength(str) 
+// console.log(x);
+
+
+// HighOrderFunctions23
+// let Obj = {"Apelsin": 10000, "Olma": 12000, "Mandarin": 8000, "Banan": 20000}
+// function Summ (obj) {
+//     let s = 0
+//     for (let i in obj) {
+//         s += obj[i]
+//     }
+//     return s
+// }
+// let x = Summ(Obj)
+// console.log(x);
+
+
+// HighOrderFunctions25
+// let obj = {abc: 1, 123: 2, d: 5}
+// function isSting (param) {
+//     let isStr = true
+//     for (let i in param) {
+//         if (!Number.isInteger(i)) {
+//             isStr = false
+//         }
+//     }
+//     return isStr
+// }
+// let x = isSting(obj)
+// console.log(x);
